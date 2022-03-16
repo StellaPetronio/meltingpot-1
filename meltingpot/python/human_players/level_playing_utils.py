24901bdbf3bfa44ba5e524c001b0e3f9daf708bc
StellaPetronio <62093435+StellaPetronio@users.noreply.github.com>
2022-03-16 09:56:50 +0100

    pygame probs

--- meltingpot/python/human_players/level_playing_utils.py
+++ meltingpot/python/human_players/level_playing_utils.py
@@ -28,13 +28,13 @@ import dm_env
 from ml_collections import config_dict
 import numpy as np
 import pygame
-#pygame.init()
+pygame.init()
 
 import dmlab2d
 from meltingpot.python.utils.substrates import builder
 
-import os
-os.environ['SDL_VIDEODRIVER']='windib'
+#import os
+#os.environ['SDL_VIDEODRIVER']='windib'
 
 WHITE = (255, 255, 255)
 
