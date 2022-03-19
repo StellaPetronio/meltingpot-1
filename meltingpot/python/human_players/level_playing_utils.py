ee4710f09b3fcb8f429d58e180b8578392fa1e62
StellaPetronio <62093435+StellaPetronio@users.noreply.github.com>
2022-03-19 11:34:15 +0100

    pygame

--- meltingpot/python/human_players/level_playing_utils.py
+++ meltingpot/python/human_players/level_playing_utils.py
@@ -36,8 +36,8 @@ from meltingpot.python.utils.substrates import builder
 
 #import os
 #os.environ['SDL_VIDEODRIVER']='windib'
-#import os
-#os.environ["SDL_VIDEODRIVER"] = "dummy"
+import os
+os.environ["SDL_VIDEODRIVER"] = "dummy"
 
 WHITE = (255, 255, 255)
 
