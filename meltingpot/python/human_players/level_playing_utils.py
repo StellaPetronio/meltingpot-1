ab0e143caad201535fb7c6b185f55c57d83c1fc7
StellaPetronio <62093435+StellaPetronio@users.noreply.github.com>
2022-03-19 09:25:25 +0100

    pygame

--- meltingpot/python/human_players/level_playing_utils.py
+++ meltingpot/python/human_players/level_playing_utils.py
@@ -28,7 +28,7 @@ import dm_env
 from ml_collections import config_dict
 import numpy as np
 import pygame
-pygame.init()
+#pygame.init()
 
 import dmlab2d
 from meltingpot.python.utils.substrates import builder
@@ -268,7 +268,7 @@ def run_episode(
   action_reader = ActionReader(env, action_map)
 
   if interactive == RenderType.PYGAME:
-    pygame.init()
+    #pygame.init()
     pygame.display.set_caption('Melting Pot: {}'.format(
         full_config.lab2d_settings.levelName))
     font = pygame.font.SysFont(None, text_font_size)
