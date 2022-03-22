7d81e88b3f5b5c6f18f02aeeb9f02571d688b339
StellaPetronio <62093435+StellaPetronio@users.noreply.github.com>
2022-03-22 21:54:13 +0100

    my substrate

--- examples/tutorial/harvest/configs/environment/harvest.py
+++ examples/tutorial/harvest/configs/environment/harvest.py
@@ -14,33 +14,152 @@
 """Configuration for tutorial level: Harvest."""
 
 from ml_collections import config_dict
+from meltingpot.python.utils.substrates import shapes
 
+#we need the spawn point in order to to see the avatars in action
+SPAWN_POINT = {
+    "name": "spawn_point",
+    "components": [
+        {
+            "component": "StateManager",
+            "kwargs": {
+                "initialState": "spawnPoint",
+                "stateConfigs": [{
+                    "state": "spawnPoint",
+                    "groups": ["spawnPoints"],
+                }],
+            }
+        },
+        {
+            "component": "Transform",
+        },
+    ]
+}
+
+AVATAR = {
+    "name": "avatar",
+    "components": [
+        {
+            "component": "StateManager",
+            "kwargs": {
+                "initialState": "player",
+                "stateConfigs": [
+                    {"state": "player",
+                     "layer": "upperPhysical",
+                     "sprite": "Avatar",},
+                ]
+            }
+        },
+        {
+            "component": "Transform",
+        },
+        {
+            "component": "Appearance",
+            "kwargs": {
+                "renderMode": "ascii_shape",
+                "spriteNames": ["Avatar"],
+                "spriteShapes": [shapes.CUTE_AVATAR],
+                #"spriteRGBColors": [(0, 0, 128)],
+                "palettes": [{}],  # Will be overridden.
+                "noRotates": [True],
+            }
+        },
+        {
+            "component": "Avatar",
+            "kwargs": {
+                "aliveState": "player",
+                "waitState": "playerWait",
+                "spawnGroup": "spawnPoints",
+                "view": {
+                    "left": 3,
+                    "right": 3,
+                    "forward": 5,
+                    "backward": 1,
+                    "centered": False,
+                }
+            }
+        },
+    ]
+}
+
+WALL = {
+    "name": "wall",
+    "components": [
+        {
+            "component": "StateManager",
+            "kwargs": {
+                "initialState": "wall",
+                "stateConfigs": [{
+                    "state": "wall",
+                    "layer": "upperPhysical",
+                    "sprite": "Wall",
+                }],
+            }
+        },
+        {
+            "component": "Transform",
+        },
+        {
+            "component": "Appearance",
+            "kwargs": {
+                "renderMode": "ascii_shape",
+                "spriteNames": ["Wall",],
+                "spriteShapes": [shapes.WALL],
+                "palettes": [shapes.WALL_PALETTE],
+                "noRotates": [True],
+            }
+        },
+        {
+            "component": "BeamBlocker",
+            "kwargs": {
+                "beamType": "gift"
+            }
+        },
+        {
+            "component": "BeamBlocker",
+            "kwargs": {
+                "beamType": "zap"
+            }
+        },
+    ]
+}
 
 def get_config():
   """Default configuration for the Harvest level."""
   config = config_dict.ConfigDict()
 
   # Basic configuration.
-  config.individual_observation_names = []
+  config.individual_observation_names = ["RGB"]
   config.global_observation_names = ["WORLD.RGB"]
 
   # Lua script configuration.
   config.lab2d_settings = {
-      "levelName":
+      "levelName": #
           "harvest",
       "levelDirectory":
-          "examples//tutorial/harvest/levels",
+          "examples//tutorial/harvest_stella/levels",
       "maxEpisodeLengthFrames":
           100,
       "numPlayers":
-          0,
+          1,
       "spriteSize":
           8,
       "simulation": {
-          "map": " ",
-          "prefabs": {},
-          "charPrefabMap": {},
+          "map": """ 
+          *******
+          *     *
+          *  -  *
+          *     *
+          *     *
+          *******
+          """,
+          "prefabs": {
+              "avatar": AVATAR,
+              "spawn_point": SPAWN_POINT,
+              "wall": WALL
+            },
+          "charPrefabMap": {"_": "spawn_point", "*": "wall"},
+          "playerPalettes": [],
       },
   }
-
   return config
